daf1cc8e6547296d5d7506556c7438567fc7e1e6
Kush2104 <kushal.1nagaraj.1@gmail.com>
2023-12-04 13:09:46 -0700

    Listing all APIs

--- app/routes/listings.py
+++ app/routes/listings.py
@@ -1,12 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from typing import List
 import os
 import pymongo
 from ..util.utils import read_json, get_mongo_collection
 from bson.json_util import dumps
-
-
+from ..model.listing import Property
 
 router = APIRouter()
 
@@ -27,3 +26,33 @@ async def get_listings():
 
     # Use JSONResponse and bson.json_util.dumps for proper serialization
     return JSONResponse(content=dumps(listings))
+
+@router.post("/add", response_model=Property)
+async def create_item(listing: Property):
+    property_model = listing.dict()
+    result = listing_collection.insert_one(property_model)
+    return {**listing.dict(), "id": str(result.inserted_id)}
+
+@router.put("/update/{property_id}", response_model=Property)
+async def update_property(property_id: int, updated_data: Property):
+    existing_data = listing_collection.find_one({"property_id": property_id})
+    if existing_data is None:
+        raise HTTPException(status_code=404, detail="Property not found")
+
+    merged_data = {**existing_data, **updated_data.dict(exclude_unset=True)}
+    listing_collection.update_one({"property_id": property_id}, {"$set": merged_data})
+    return merged_data
+
+@router.delete("/delete/{property_id}")
+async def delete_property(property_id: int):
+    existing_data = listing_collection.find_one({"property_id": property_id})
+    if existing_data is None:
+        raise HTTPException(status_code=404, detail="Property not found")
+
+    # Perform the delete operation
+    result = listing_collection.delete_one({"property_id": property_id})
+
+    if result.deleted_count == 1:
+        return {"status": "success", "message": "Property deleted successfully"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to delete property")
