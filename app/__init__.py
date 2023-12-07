206530b45d19fad6abe8494254b64958bc7d2b4c
Anirudh Maiya <maiyaanirudh@gmail.com>
2023-12-07 15:27:35 -0700

    add route

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .routes import listings
 from .routes import home
+from .routes import auth
 
 app_router = APIRouter()
 app_router.include_router(listings.router, prefix="/listings", tags=["listings"])
 app_router.include_router(home.router, tags=["home_page"])
+app_router.include_router(auth.router, prefix="/auth", tags=["token"])
